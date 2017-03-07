2d53e2d64403c0c8c1db4decee332b34e7562a1d
trenjohn <trent.matt.j@gmail.com>
2017-03-07 08:36:39 -0700

    Allow transition to /dashboard/office if admin.

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -6,19 +6,17 @@ export default Ember.Route.extend({
   actions: {
     willTransition(transition) {
       if (transition.targetName === 'dashboard.index'){
-        //console.log('Trying to go to /dashboard');
-        //console.log("DEBUG2: Router.didTransition");
         const uid = this.get('firebaseApp').auth().currentUser.uid;
         this.store.query('profile', {orderBy: 'uid', equalTo: uid}).then((users) => {
             const user = users.get('firstObject');
             var role = user.get('role');
             var firstLogin = user.get('firstLogin');
-            //console.log(firstLogin);
             if (firstLogin === '0') {
               this.transitionTo('update-password');
+            } else if (role === "admin") {
+              this.transitionTo('/dashboard/office/home');
             } else {
-            //console.log(role);
-            //console.log('beforeModel');
+
             switch (role)
               {
                case "office":
@@ -43,31 +41,26 @@ export default Ember.Route.extend({
         const user = users.get('firstObject');
         var role = user.get('role');
         var firstLogin = user.get('firstLogin');
-        //console.log(firstLogin);
         if (firstLogin === '0') {
-          //console.log('Redirecting to update password');
           this.transitionTo('update-password');
         } else {
-        //console.log(role);
-        //console.log('beforeModel');
-        switch (role)
-          {
-           case "office":
-              this.transitionTo('/dashboard/office/home');
-              break;
-           case "admin":
-              this.transitionTo('/dashboard/admin/home');
-              break;
-           case "foreman":
-              this.transitionTo('/dashboard/foreman/home');
-              break;
-          }
+          switch (role)
+            {
+             case "office":
+                this.transitionTo('/dashboard/office/home');
+                break;
+             case "admin":
+                this.transitionTo('/dashboard/admin/home');
+                break;
+             case "foreman":
+                this.transitionTo('/dashboard/foreman/home');
+                break;
+            }
       }
     });
   },
   model() {
     const uid = this.get('firebaseApp').auth().currentUser.uid;
-    //console.log('model');
     return this.get('store').query('profile', {orderBy: 'uid', equalTo: uid});
   }
 });
